fc7bccebdc2aed2589949ef6cedf2a9f9a6c3790
Felipe <felipe.arosa@gmail.com>
2022-11-29 18:54:01 -0300

    created hero and started carousel with swiper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,19 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: '2',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+console.log('ivew')

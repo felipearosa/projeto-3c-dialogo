e483ed2ee0d6ef367c5b8e20508300ab109076f0
Felipe <felipe.arosa@gmail.com>
2022-11-30 13:23:59 -0300

    test: cards carousel working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,13 +8,17 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  slidesPerView: '2',
+  slidesPerView: '4',
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
+  }
+
 });
 
 console.log('ivew')
+
+
+// 'cube' | 'coverflow' | 'flip' | 'creative' | 'cards'
